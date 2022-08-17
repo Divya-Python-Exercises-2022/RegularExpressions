f1609b665b14ca2f09938a06a35752f73caa0881
divya chandran <divya.chandran10@gmail.com>
2022-08-17 09:08:47 +0200

    Add task6

--- StringRegex1/Task6.py
+++ StringRegex1/Task6.py
@@ -2,6 +2,5 @@ import re
 
 if __name__ == '__main__':
     text = "The rain in Spain."
-    match = re.search(r'.*ai.*', text)
-    print(match.group(0))
-    #print(match.count('ai'))
+    match = re.findall(r'.ai.', text)
+    print(len(match))
